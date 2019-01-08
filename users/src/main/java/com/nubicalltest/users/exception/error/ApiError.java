d615147d69f0c5e4c742156dab4dec36f4515dfb
Marcelo Wieja <mdw.arg@gmail.com>
2019-01-08 18:57:07 -0300

    Improve error response messagges

---
+++ users/src/main/java/com/nubicalltest/users/exception/error/ApiError.java
@@ -0,0 +1,66 @@
+package com.nubicalltest.users.exception.error;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+	private HttpStatus status;
+
+	private String uuid;
+
+	private String message;
+
+	private List<String> errors;
+
+	public ApiError(HttpStatus status, String message, List<String> errors) {
+		super();
+		this.status = status;
+		this.uuid = UUID.randomUUID().toString();
+		this.message = message;
+		this.errors = errors;
+	}
+
+	public ApiError(HttpStatus status, String message, String error) {
+		super();
+		this.status = status;
+		this.uuid = UUID.randomUUID().toString();
+		this.message = message;
+		this.errors = Arrays.asList(error);
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+}
