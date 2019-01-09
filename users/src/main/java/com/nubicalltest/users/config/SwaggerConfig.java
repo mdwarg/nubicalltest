3717067e0635500e8ff730ecc3b9c573bbd77929
Marcelo Wieja <mdw.arg@gmail.com>
2019-01-09 00:00:11 -0300

    Swagger add schema api key

--- users/src/main/java/com/nubicalltest/users/config/SwaggerConfig.java
+++ users/src/main/java/com/nubicalltest/users/config/SwaggerConfig.java
@@ -1,29 +1,18 @@
 package com.nubicalltest.users.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.collect.Lists;
+
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
-import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.GrantType;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.service.TokenEndpoint;
-import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -32,22 +21,19 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
+		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.nubicalltest.users.controller"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(apiInfo());
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo()).securitySchemes(Lists.newArrayList(apiKey()));
 	}
-	
+
 	private ApiInfo apiInfo() {
-	    return new ApiInfoBuilder()
-	    		.title("User API")
-	    		.description("User API specification")
-	    		.version("1.0.0")
-	    		.license("MIT")
-	    		.licenseUrl("https://opensource.org/licenses/MIT")
-	    		.contact(new Contact("Marcelo Wieja", "https://linkedin.com/in/marcelo-david-wieja/", "marcelo.wieja@gmail.com"))
-	    		.build();
+		return new ApiInfoBuilder().title("User API").description("User API specification").version("1.0.0")
+				.license("MIT").licenseUrl("https://opensource.org/licenses/MIT").contact(new Contact("Marcelo Wieja",
+						"https://linkedin.com/in/marcelo-david-wieja/", "marcelo.wieja@gmail.com"))
+				.build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "api_key", "header");
 	}
 }
